244296c5d0236b37e9b899ba46fd852cd9485c07
alexn123 <alexn@tlu.ee>
2021-09-12 20:00:08 +0300

    kodutoo

--- backend/src/main/java/ee/alex/backend/controller/CategoryController.java
+++ backend/src/main/java/ee/alex/backend/controller/CategoryController.java
@@ -1,4 +1,28 @@
 package ee.alex.backend.controller;
 
+
+import ee.alex.backend.model.Category;
+import ee.alex.backend.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
+
+    @GetMapping("categories")
+    public List<Category> getCategories(){ return categoryService.getcategories(); }
+
+    @PostMapping("categories")
+    public String postCategory(@RequestBody Category category) {
+        categoryService.saveCategory(category);
+        return "kategooria edukalt lisatud " + category.getCategory();
+    }
 }
--- backend/src/main/java/ee/alex/backend/repository/CategoryRepository.java
+++ backend/src/main/java/ee/alex/backend/repository/CategoryRepository.java
@@ -1,4 +1,10 @@
 package ee.alex.backend.repository;
 
-public interface CategoryRepository {
+
+import ee.alex.backend.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
